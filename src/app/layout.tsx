b000431dc836d084796d06e364488867bbf514b6
George Bobby <mailto.georgebobby@gmail.com>
2024-12-22 09:27:39 +0530

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <Header />
         <main className="p-8">
           {children}
